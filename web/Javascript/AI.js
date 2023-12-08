cbce7e46d59e5133ae12f73f9ee41b300347532a
WarezNavzdy <warezovaadresa@centrum.cz>
2023-12-08 15:55:09 +0100

    Tonda je buh

--- web/Javascript/AI.js
+++ web/Javascript/AI.js
@@ -1,5 +1,8 @@
 let sirky;
 let displaySirky = document.getElementById('grafika');
+const nigga1 = document.getElementById("nigga1");
+const nigga2 = document.getElementById("nigga2");
+let kolikDam;
 
 function input(params) {
     sirky = document.getElementById('kockajebuh').value;
@@ -11,7 +14,19 @@ function input(params) {
     document.getElementById('input').style.display = 'none';
     sýrek();
     if(sirky % 4 == 1){
-        document.getElementById("")
+        nigga1.textContent = "Seš na řadě, tak hrej pičo!";
+    } else {
+        if(sirky % 4 == 0){
+            kolikDam = 3;
+        } else {
+        kolikDam = (sirky % 4) - 1;
+        }
+        
+        nigga1.textContent = "Začínáme tedy s "+ sirky + "! Beru " + kolikDam + "!";
+        
+        sirky -= kolikDam;
+        sýrek();
+        nigga2.textContent = "Dohromady máme teď sirek " + sirky + "! Hraješ ty!";
     }
         }
 }
@@ -21,4 +36,11 @@ function sýrek(params) {
     for(let i = 1; i <= sirky; i++){
         displaySirky.innerHTML += "<img src='/web/images/sirkav2.svg' alt=''>";
     }
+}
+
+function pridej(kolik){
+    nigga1.textContent = nigga2.textContent;
+    sirky = sirky - kolik;
+    sýrek();
+    nigga2.textContent = "Dobře, dáváš " + kolik + "sirek, dohromady teda máme " + sirky + "sirek!";
 }
