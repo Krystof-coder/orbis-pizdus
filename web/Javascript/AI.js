b38b10014ea0a87e3e1908aadb56cc6920eff2b3
Krystof-coder <krystof.4code@seznam.cz>
2023-12-11 11:45:43 +0100

    final pico

--- web/Javascript/AI.js
+++ web/Javascript/AI.js
@@ -39,8 +39,27 @@ function sýrek(params) {
 }
 
 function pridej(kolik){
+    if(sirky - kolik > 0){
     nigga1.textContent = nigga2.textContent;
     sirky = sirky - kolik;
     sýrek();
-    nigga2.textContent = "Dobře, dáváš " + kolik + "sirek, dohromady teda máme " + sirky + "sirek!";
+    nigga2.textContent = "Dobře, dáváš " + kolik + " sirek, dohromady teda máme " + sirky + " sirek!";
+    kolikDam = 4 - kolik;
+
+    document.getElementById("jepka").innerHTML = '        <button class="btn">1</button><button class="btn" >2</button><button class="btn" >3</button>';
+    setTimeout(function(){
+        nigga1.textContent = nigga2.textContent;
+        sirky -= kolikDam;
+        nigga2.textContent = "Já tam beru pico " + kolikDam + " sirek. Dohromady potom máme " + sirky + " sirek";
+        sýrek();
+        document.getElementById("jepka").innerHTML = '        <button class="btn" onclick="pridej(1)">1</button><button class="btn" onclick="pridej(2)">2</button><button class="btn" onclick="pridej(3)">3</button>';
+    }, 1000);
+}else{
+    nigga2.textContent = "EZ vyhrál jsem (zase)";
+    sirky = 0;
+    sýrek();
+    setTimeout(function() {
+        document.getElementById('input').style.display = 'flex';
+    }, 5555);
+}
 }
